b5fab41f5c8ae1edaebb2fc1701812ec21a97259
marcus-vfr <minerio67@gmail.com>
2026-06-05 14:17:06 -0300

    Create validador.py

---
+++ FastDelivery-Express/util/validador.py
@@ -0,0 +1,33 @@
+from modelos.entregador import Entregador
+
+
+class EntregadorService:
+    """
+    Responsável pelo gerenciamento dos entregadores.
+    """
+
+    def __init__(self):
+        self.__entregadores = []
+
+    def cadastrar_entregador(self, nome, veiculo, cnh):
+
+        entregador = Entregador(
+            nome,
+            veiculo,
+            cnh
+        )
+
+        self.__entregadores.append(entregador)
+
+        return entregador
+
+    def listar_entregadores(self):
+        return self.__entregadores
+
+    def buscar_entregador_por_cnh(self, cnh):
+
+        for entregador in self.__entregadores:
+            if entregador.cnh == cnh:
+                return entregador
+
+        return None
